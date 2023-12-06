e1e80191ef65008d740c104ce0267144a1295888
Abdus samad <abdussamadkotha@gmail.com>
2023-12-06 22:47:56 +0500

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-export default function App() {
+import {
+  Hero,
+  CustomerReview,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality
+} from './sections'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReview />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className='bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
   )
 }
+
+export default App
