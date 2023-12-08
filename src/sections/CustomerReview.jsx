07a6af81538e325e5cb07c96c751cfeaf4d8fcd2
Abdus samad <abdussamadkotha@gmail.com>
2023-12-08 11:58:22 +0500

    Customer Review

--- src/sections/CustomerReview.jsx
+++ src/sections/CustomerReview.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import ReviewCard from '../components/ReviewCard'
+import { reviews } from '../constants'
 const CustomerReview = () => {
   return (
-    <div>CustomerReview</div>
+    <section className='max-container'>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>What Our
+      <span className='text-coral-red'>Customers</span> Say?
+      </h3>
+      <p className='info-text m-auto mt-4 max-w-lg text-center'>
+        Hear genuine stories from our satisfied customers about their exceptional experience with us.
+      </p>
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {
+          reviews.map((review) => (
+            <ReviewCard 
+              key={review.customerName}
+              imgURL={review.imgURL}
+              customerName={review.customerName}
+              feedback={review.feedback}
+              rating={review.rating}
+            />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
