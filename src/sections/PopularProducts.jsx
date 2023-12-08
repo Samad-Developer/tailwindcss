c5d8be6becb396932f720ccd04f5591284e7b775
Abdus samad <abdussamadkotha@gmail.com>
2023-12-08 17:06:36 +0500

    Footer

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,10 +1,12 @@
 import { products } from '../constants'
+
+
 import PopularProductCard from '../components/PopularProductCard'
 const PopularProducts = () => {
   return (
     <section
-    id='products' 
-    className='max-container max-sm:mt-12'
+      id='products'
+      className='max-container max-sm:mt-12'
     >
       <div className='flex flex-col justify-start gap-5'>
         <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
@@ -13,13 +15,14 @@ const PopularProducts = () => {
       <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14'>
         {
           products.map((product) => (
-            <PopularProductCard 
+            <PopularProductCard
               key={product.name}
               {...product}
             />
           ))
         }
       </div>
+
     </section>
   )
 }
